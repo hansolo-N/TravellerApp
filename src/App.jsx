12329a851080a99f5cf04eaddda0f3fb9c59cde4
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-10-10 11:24:42 +0200

    intialized travelling app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route} from 'react-router-dom'
+
+function Traveller() {
+
+ 
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path=''></Route>
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Traveller

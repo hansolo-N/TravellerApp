3cc6cfec8be4f18b399fd9a90523f32f94c177a3
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-10-10 12:15:33 +0200

    created main app component

--- src/components/NavPage.jsx
+++ src/components/NavPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from "./NavPage.module.css"
 
 function NavPage() {
   return (
-    <nav>
+    <nav className={styles.nav}>
         <ul>
             <li><Link to={"/"}>Home</Link></li>
             <li><Link to={"pricing"}>Pricing</Link></li>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavPage from '../components/NavPage'
+import { Link } from 'react-router-dom'
 
 function Homepage() {
   return (
     <div>
       <NavPage/>
       Homepage
+      <Link to={"/app"}>go to the app</Link>
       </div>
   )
 }

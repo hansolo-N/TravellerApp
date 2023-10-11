fa48392f1f53678f680981344e4794227b20d8cf
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-10-11 10:26:54 +0200

    added more dsiplay city details in city item component

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import styles from "./CityList.module.css"
 import Spinner from '../components/Spinner'
 import CityItem from './CityItem'
+import Message from './Message'
 
 function CityList({cities,isLoading}) {
 console.log(cities)
 
 if(isLoading) return <Spinner/>
+
+if(!cities.length)
+    return <Message message={'add your first your first city by clicking on some location on the map'}/>
+
+
   return (
     <ul className={styles.cityList}>
         {cities.map((city)=>

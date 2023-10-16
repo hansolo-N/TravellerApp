ce084056140a5ec24b6b61c7f9f08f06d10c358d
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-10-16 14:54:47 +0200

    added new state and conditional rendering for geolocation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,12 @@
-// "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
+import "react-datepicker/dist/react-datepicker.css";
 import { useEffect, useState} from "react";
 import styles from "./Form.module.css";
 import Button from "./Button";
 import BackButton from "./BackButton";
 import { useUrlCoordinates } from "../hooks/useUrlCoordinates";
+import Message from "./Message"
+import DatePicker from "react-datepicker";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -20,13 +22,55 @@ function Form() {
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
+  const [isLoadingGeoData,setIsLoadingGeoData] = useState(false)
+  const [emoji,setEmoji] = useState()
+  const [geoLocationError,setGeoLocationError] = useState('')
+
+  const baseUrl = "https://api.bigdatacloud.net/data/reverse-geocode-client"
 
   useEffect(function(){
-    async function fetch
-  },[])
+    if(!lat && !lng) return;
 
+    async function fetchCity(){
+      try {
+        setIsLoadingGeoData(true)
+        setGeoLocationError('')
+        const response = await fetch(`${baseUrl}?latitude=${lat}&longitude=${lng}`)
+        const data = await response.json()
+
+        if(!data.countryCode){
+          throw new Error("that does not seem to be a city, try another location 😁")
+        }
+
+        setCityName(data.city || data.locality ||"")
+        setCountry(data.countryName)
+        setEmoji(convertToEmoji(data.countryCode))
+      } catch (error) {
+        setGeoLocationError(error.message)
+      }
+      finally{
+        setIsLoadingGeoData(false)
+      }
+    }
+    fetchCity()
+  },[lat,lng])
+
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+  }
+
+
+  if(geoLocationError){
+    return <Message message={geoLocationError}/>
+  }
+
+  if(!lat && !lng){
+     return <Message message ={"click somwhere on the map"}/>
+  }
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={()=>handleSubmit}>
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
@@ -34,16 +78,18 @@ function Form() {
           onChange={(e) => setCityName(e.target.value)}
           value={cityName}
         />
-        {/* <span className={styles.flag}>{emoji}</span> */}
+        <span className={styles.flag}>{emoji}</span>
       </div>
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
+        {/* <input
           id="date"
           onChange={(e) => setDate(e.target.value)}
           value={date}
-        />
+        /> */}
+        <DatePicker onChange={(date)=>setDate(date)} selected={date} dateFormat='dd/MM/yyyy'/>
+        
       </div>
 
       <div className={styles.row}>
